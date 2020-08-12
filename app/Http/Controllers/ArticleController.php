80eb10367a915a86da3177c8f7a8b2d224235e39
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-08-12 17:19:42 +0500

    Added delete feature

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,12 +8,26 @@ use Illuminate\Support\Facades\Validator;
 class ArticleController extends Controller
 {
     function Show(){
-        $articles = DB::table('articles')->orderBy('id','DESC')->get();
+        $articles = DB::table('articles')->orderBy('id','DESC')->get(); //query builder method
+        //u can also use orm method 
+        //$articles = Article::orderBy('id','DESC')->all()->get();
         return view('list')->with(compact('articles'));
     }
 
     function Add(){
         return view('Add');
+    }
+    function delete($id, Request $request){
+        $article = Articles::where('id',$id)->first();
+        if(!$article){
+            $request->session()->flash('errmsg','Article not found');            
+            return redirect('articles');
+        }
+        else{
+            Articles::where('id',$id)->delete();            
+            $request->session()->flash('sucmsg','Article deleted');            
+            return redirect('articles');            
+        }
     }
     function save(Request $request){
         $validatedData = Validator::make($request->all(),[
