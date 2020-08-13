4a5f35639ce9c442af3f1fa8af420ed91745c711
Muhammad Faraz Ansar <49414010+muhammadfaraz6640@users.noreply.github.com>
2020-08-14 00:10:03 +0500

    updated feature added

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -29,6 +29,34 @@ class ArticleController extends Controller
             return redirect('articles');            
         }
     }
+    function update(int $id, Request $request){
+        $validatedData = Validator::make($request->all(),[
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'author' => 'required|max:100'
+        ]);
+        if($validatedData->passes()){
+            //insert into database            
+                $articles = new App\Articles::find(1);
+                $articles->title = $request->title;
+                $articles->author = $request->author;
+                $articles->description = $request->description;
+                $articles->save();
+                $request->session()->flash('msg','Articles updated successfully');
+                return redirect('articles');
+        }
+        else{
+            return redirect('articles')->withErrors($validatedData)->withInput();
+        }
+    }
+    function edit($id, Request $request){
+        $article = Articles::where('id',$id)->first();
+        if(!$article){
+            $request->session()->flash('erreditmsg','Article not found');            
+            return redirect('articles');
+        }
+        return view('edit')->with(compact('article'));
+    }
     function save(Request $request){
         $validatedData = Validator::make($request->all(),[
             'title' => 'required|max:255',
